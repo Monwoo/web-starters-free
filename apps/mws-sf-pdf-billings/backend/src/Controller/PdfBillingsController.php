b5b6ffbf73b6abe6ce7a83f9dacab2a046cd21d0
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-06-19 05:43:39 +0200

    [MWS][mws-sf-pdf-billings] using TWIG template as pdf content source OK

--- apps/mws-sf-pdf-billings/backend/src/Controller/PdfBillingsController.php
+++ apps/mws-sf-pdf-billings/backend/src/Controller/PdfBillingsController.php
@@ -55,6 +55,7 @@ class PdfBillingsController extends AbstractController
         }
 
         // return $this->render('pdf-billings/index.html.twig', [
+        //     'form' => $form->createView(),
         return $this->renderForm('pdf-billings/index.html.twig', [
             'bConfig' => $bConfig,
             'form' => $form,
@@ -64,8 +65,15 @@ class PdfBillingsController extends AbstractController
 
     #[Route('/view', name: 'app_pdf_billings_view')]
     public function view(
+        // EngineInterface $tplEngine,
         BillingConfigRepository $bConfigRepository,
     ) : Response {
+        // Missing deps injections in new version ?
+        // Symfony\Bundle\FrameworkBundle\Templating\EngineInterface $tplEngine ?
+        // $html = $tplEngine->render('pdf-billings/pdf-views/monwoo-quotation.html.twig', [
+
+        $twig = $this->container->get('twig');
+
         $bConfig = $bConfigRepository->findOneBy([]);
         $pdf = $this->tcpdf->create();
 
@@ -84,16 +92,9 @@ class PdfBillingsController extends AbstractController
         $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
 
         // Set some content to print
-        $html = <<<EOD
-        {$bConfig->getClientName()}
-        // Welcome to mws-sf-pdf-billings
-        // EOD;
-        <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-        <i>This is the first example of TCPDF library.</i>
-        <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-        <p>Please check the source code documentation and other examples for further information.</p>
-        <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
-        EOD;
+        $html = $twig->render('pdf-billings/pdf-views/monwoo-quotation.html.twig', [
+            'bConfig' => $bConfig,
+        ]);
 
         $pdf->AddPage();
         // Print text using writeHTMLCell()
