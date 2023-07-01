c1bc04d2a75a84c936d059077651d81214e31031
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-07-01 13:33:14 +0200

    [MWS][mws-sf-pdf-billings] adding debugger toolbar and profiler for dev environements

--- apps/mws-sf-pdf-billings/backend/config/bundles.php
+++ apps/mws-sf-pdf-billings/backend/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
