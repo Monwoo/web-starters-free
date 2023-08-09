d56a03251fef1b1328595bb29cd6b34c219e9b5b
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-08-09 10:26:53 +0200

    [MWS][mws-sf-pdf-billings] improving Factory Reset timeout

--- apps/mws-sf-pdf-billings/backend/src/Controller/FactoryResetController.php
+++ apps/mws-sf-pdf-billings/backend/src/Controller/FactoryResetController.php
@@ -9,8 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FactoryResetController extends AbstractController
 {
-    #[Route('/factory/reset', name: 'app_factory_reset')]
-    public function index(string $projectDir, LoggerInterface $logger): JsonResponse
+    #[Route(
+        '/factory/reset/{forceTimeout}',
+        defaults: [
+            'forceTimeout' => false,
+        ],
+        name: 'app_factory_reset'
+    )]
+    public function index(bool $forceTimeout, string $projectDir, LoggerInterface $logger): JsonResponse
     {
         $msg = '';
         $didFail = false;
@@ -27,28 +33,39 @@ class FactoryResetController extends AbstractController
             copy($safeGdprDatabase, $database);
             $msg .= 'Resolution in progress : Did create database. ';
         }
-        $serverClock = new \DateTime();
-        // https://stackoverflow.com/questions/13386082/filemtime-warning-stat-failed-for
-        $db_creation_timestamp = filectime(realpath($database));
-        // $stat = stat($database);
-        // $db_creation_timestamp = $stat['ctime']; // date_create(date("Y-m-d", $stat['ctime']));
 
-        $next_possible_reset_date = new \DateTime();
-        $next_possible_reset_date->setTimestamp($db_creation_timestamp);
-        // TIPS : allow factory reset only each 5 minutes to avoid service cleanups overloads
-        $next_possible_reset_date->add(new \DateInterval('PT5M'));
-        if (
-            ($serverClock)->getTimestamp()
-            > $next_possible_reset_date->getTimestamp()
+        $clean = function () use (
+            $msg, $database, $safeGdprDatabase, $dbBackup
         ) {
             copy($database, $dbBackup);
             unlink($database);
             copy($safeGdprDatabase, $database);
             $msg .= 'OK : Did resset this application to factory settings. ';
+        };
+        $serverClock = new \DateTime();
+        $next_possible_reset_date = new \DateTime();
+        // TODO : add param config on allowed IP for production ENV ?
+        if ($forceTimeout && 'dev' === getenv('APP_ENV')) {
+            $clean();
         } else {
-            $didFail = true;
-            // TODO : e2e tests on user messages realistic with timings ? (using time stubs etc...)
-            $msg .= 'ERROR : Please wait at most 5 minutes before next factory reset is available. ';
+            // https://stackoverflow.com/questions/13386082/filemtime-warning-stat-failed-for
+            $db_creation_timestamp = filectime(realpath($database));
+            // $stat = stat($database);
+            // $db_creation_timestamp = $stat['ctime']; // date_create(date("Y-m-d", $stat['ctime']));
+
+            $next_possible_reset_date->setTimestamp($db_creation_timestamp);
+            // TIPS : allow factory reset only each 5 minutes to avoid service cleanups overloads
+            $next_possible_reset_date->add(new \DateInterval('PT5M'));
+            if (
+                ($serverClock)->getTimestamp()
+                > $next_possible_reset_date->getTimestamp()
+            ) {
+                $clean();
+            } else {
+                $didFail = true;
+                // TODO : e2e tests on user messages realistic with timings ? (using time stubs etc...)
+                $msg .= 'ERROR : Please wait at most 5 minutes before next factory reset is available. ';
+            }
         }
 
         return $this->json([
