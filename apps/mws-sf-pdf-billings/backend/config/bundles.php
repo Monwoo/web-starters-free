351a752a711fd7d625d8fa259a8fe16949ce9669
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-10-07 14:13:24 +0200

    [MWS][mws-sf-pdf-billings] adding 'encore-bundle'

--- apps/mws-sf-pdf-billings/backend/config/bundles.php
+++ apps/mws-sf-pdf-billings/backend/config/bundles.php
@@ -13,7 +13,7 @@ $bundles = [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     MWS\PDFBillingsLvl1Bundle\PDFBillingsLvl1Bundle::class => ['all' => true],
-    MWS\MoonManagerBundle\MoonManagerBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
 
 // Paid or private starter might not be loaded, do lazy loads :
