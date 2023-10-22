e87731f3a48288d789cb44a4cdf2df80c5fd07d2
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2023-10-22 05:27:04 +0200

    [MWS][mws-moon-manager][mws-pdf-billings] improving tags system in progress

--- packages/mws-moon-manager-ux/components/menu/NavBar.svelte
+++ packages/mws-moon-manager-ux/components/menu/NavBar.svelte
@@ -146,7 +146,15 @@
                   href={Routing.generate("mws_offer_tags")}
                   class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 >
-                  Tags</a
+                  Offers Tags</a
+                >
+              </li>
+              <li>
+                <a
+                  href={"#TODO-CalendarEventTags" || Routing.generate("mws_offer_tags")}
+                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Calendar Tags</a
                 >
               </li>
               <li>
