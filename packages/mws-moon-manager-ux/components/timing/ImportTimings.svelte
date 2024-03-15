39d9537004076fa247165b83b0bae0c3b8d2ed4b
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2024-03-15 11:48:58 +0100

    [MWS][mws-moon-manager][mws-pdf-billings] in progress, timings import improved

--- packages/mws-moon-manager-ux/components/timing/ImportTimings.svelte
+++ packages/mws-moon-manager-ux/components/timing/ImportTimings.svelte
@@ -11,48 +11,53 @@
   export let importedTagsGrouped;
   export let locale;
   export let availableFormat = [
-    { format:'json', label:'JSON' },
-    { format:'yaml', label:'YAML' },
-    { format:'csv', label:'CSV' },
+    { format: "json", label: "JSON" },
+    { format: "yaml", label: "YAML" },
+    { format: "csv", label: "CSV" },
   ];
-  export let format = 'yaml';
+  export let format = "yaml";
   // 'application/xml',
   // 'application/json',
   // 'application/yaml',
   // 'application/csv',
   // 'text/csv',
   export let formatToMime = {
-    'json': 'application/json,text/plain',
-    'yaml': 'application/yaml,text/plain,application/x-yaml',
-    'csv': 'application/csv,text/csv',
-  }
+    json: "application/json,text/plain",
+    yaml: "application/yaml,text/plain,application/x-yaml",
+    csv: "application/csv,text/csv",
+  };
 
-  export let importTags = async (inputData = {
-    format: 'yaml', importFile: null, shouldOverwrite: null
-  }) => {
+  export let importTags = async (
+    inputData = {
+      format: "yaml",
+      importFile: null,
+      shouldOverwrite: null,
+    }
+  ) => {
     // shouldOverwrite is null or 'on'
     const data = {
-      _csrf_token: stateGet(get(state), 'csrfTimingImport'),
+      _csrf_token: stateGet(get(state), "csrfTimingImport"),
       // format, shouldOverwrite,
       // importFile: importFile, // JSON.stringify(timeTag),
-      ...inputData
+      ...inputData,
     };
-		// let headers:any = {}; // { 'Content-Type': 'application/octet-stream', 'Authorization': '' };
-		let headers = {};
+    // let headers:any = {}; // { 'Content-Type': 'application/octet-stream', 'Authorization': '' };
+    let headers = {};
     // https://stackoverflow.com/questions/35192841/how-do-i-post-with-multipart-form-data-using-fetch
     // https://muffinman.io/uploading-files-using-fetch-multipart-form-data/
-    // Per this article make sure to NOT set the Content-Type header. 
-		// headers['Content-Type'] = 'application/json';
-    const formData  = new FormData();      
-    for(const name in data) {
+    // Per this article make sure to NOT set the Content-Type header.
+    // headers['Content-Type'] = 'application/json';
+    const formData = new FormData();
+    for (const name in data) {
       formData.append(name, data[name]);
     }
     const resp = await fetch(
       // TODO : build back Api, will return new csrf to use on success, will error othewise,
       // if error, warn user with 'Fail to remove tag. You are disconnected, please refresh the page...'
-      Routing.generate('mws_timing_import', {
+      Routing.generate("mws_timing_import", {
         _locale: locale,
-      }), {
+      }),
+      {
         method: "POST",
         headers,
         // body: JSON.stringify(data), // TODO : no automatic for SF to extract json in ->request ?
@@ -60,14 +65,15 @@
         // https://stackoverflow.com/questions/34558264/fetch-api-with-cookie
         credentials: "same-origin",
         // https://javascript.info/fetch-api
-        redirect: 'error',
+        redirect: "error",
       }
-    ).then(async resp => {
-      console.log(resp);
-      if (!resp.ok) {
-        // make the promise be rejected if we didn't get a 2xx response
-        throw new Error("Not 2xx response", {cause: resp});
-      } else {
+    )
+      .then(async (resp) => {
+        console.log(resp);
+        if (!resp.ok) {
+          // make the promise be rejected if we didn't get a 2xx response
+          throw new Error("Not 2xx response", { cause: resp });
+        } else {
           // got the desired response
           const data = await resp.json();
           console.debug("Did import", data);
@@ -79,61 +85,87 @@
           stateUpdate(state, {
             csrfTimingImport: data.newCsrf,
           });
-      }
-    }).catch(e => {
-      console.error(e);
-      // TODO : in secure mode, should force redirect to login without message ?, and flush all client side data...
-      const shouldWait = confirm("Echec de l'enregistrement.");
-    });
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+        // TODO : in secure mode, should force redirect to login without message ?, and flush all client side data...
+        const shouldWait = confirm("Echec de l'enregistrement.");
+      });
   };
-
 
   const submit = async (e) => {
     const formData = new FormData(e.target);
     const formJson = Object.fromEntries(formData.entries());
-    console.log('Should import timings : ', formJson);
+    console.log("Should import timings : ", formJson);
     importTags(formJson);
+  };
+
+  // https://stackoverflow.com/questions/69913703/processing-files-from-an-input-element
+  // https://svelte.dev/examples/file-inputs
+  function handleFileChange(event) {
+    let files = event.target.files;
+    for (let f of files) {
+      const ext = f.name.split('.').slice(-1)[0];
+      console.log(f, ext);
+      if (format != ext && (formatToMime[ext] ?? null)) {
+        format = ext;
+      }
+    }
   }
+
 </script>
 
 <form on:submit|preventDefault={submit} class="mws-import-timings-form">
-  <input type="file" name="importFile" accept={formatToMime[format]} />
+  {#if format}
+    <input
+      type="file"
+      name="importFile"
+      accept={formatToMime[format]}
+      on:change={handleFileChange}
+    />
+  {/if}
   <br />
   <select
-  bind:value={format}
-  name="format"
-  class="opacity-30 hover:opacity-100 
+    bind:value={format}
+    name="format"
+    class="opacity-30 hover:opacity-100 
   bg-gray-50 border border-gray-300 text-gray-900 
   text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
   inline-flex w-[10rem] p-1 m-2 dark:bg-gray-700 dark:border-gray-600 
   dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
-  dark:focus:border-blue-500">
+  dark:focus:border-blue-500"
+  >
     <option value="null" selected>Format d'import</option>
     {#each availableFormat as fmt}
       <option value={`${fmt.format}`}>{fmt.label}</option>
     {/each}
-  </select>  
+  </select>
   <!-- <input type="checkbox" name="shouldOverwrite" checked /> -->
   <span class="inline-flex flex-col">
     <span>
       <input type="checkbox" name="shouldOverwrite" />
-      <label for="shouldOverwrite">Forcer la surcharge des temps</label>  
+      <label for="shouldOverwrite">Forcer la surcharge des temps</label>
     </span>
     <span>
       <input type="checkbox" name="shouldOverwritePriceRules" />
-      <label for="shouldOverwrite">Forcer la surcharge des règles de prix</label>  
+      <label for="shouldOverwrite">Forcer la surcharge des règles de prix</label
+      >
     </span>
   </span>
   <span class="inline-flex flex-col">
     <span>
       <input type="checkbox" name="shouldRecomputeAllOtherTags" />
-      <label for="shouldOverwrite">Mettre à jour les tags des autres temps</label>  
+      <label for="shouldOverwrite"
+        >Mettre à jour les tags des autres temps</label
+      >
     </span>
     <span>
       <input type="checkbox" name="shouldIdentifyByFilename" />
-      <label for="shouldIdentifyByFilename">Identifier par nom de fichier</label>  
+      <label for="shouldIdentifyByFilename">Identifier par nom de fichier</label
+      >
     </span>
-</span>
+  </span>
 
   <button type="submit" class="btn btn-outline-primary p-1 m-1">
     Importer les temps
@@ -141,7 +173,7 @@
   <!-- <input type="submit" class="btn btn-outline-primary p-1 m-1"/>     -->
 </form>
 {#if importReport?.length}
-<div>
-  {@html importReport}
-</div>
+  <div>
+    {@html importReport}
+  </div>
 {/if}
