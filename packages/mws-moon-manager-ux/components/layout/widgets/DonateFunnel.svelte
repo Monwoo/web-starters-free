d0ff6e0bf00e296451f51d8e865c10b0bf3f36d4
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2024-04-09 06:28:51 +0200

    [MWS][mws-moon-manager][mws-pdf-billings] in progress, improving donate funnel

--- packages/mws-moon-manager-ux/components/layout/widgets/DonateFunnel.svelte
+++ packages/mws-moon-manager-ux/components/layout/widgets/DonateFunnel.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   // 🌖🌖 Copyright Monwoo 2024 🌖🌖, build by Miguel Monwoo, service@monwoo.com
   import { onMount } from "svelte";
+  import debounce from 'lodash/debounce';
   // import dayjs from "dayjs";
 
   // https://medium.com/william-joseph/things-i-wish-id-known-when-starting-website-projects-9133a5188878
@@ -14,17 +15,52 @@
   // https://page.funnel.io/elevate-your-reporting
   // https://charityfunnel.io/ => TODO : more advanced than custom paypal ? only for specific known domains ?
   //
+  const urlParams = new URLSearchParams(window.location.search);
+
+  export let affiliationCode = urlParams.get("affiliationCode") ?? null;
+  export let userDelay = 200;
+
   onMount(() => {});
 </script>
 
-<div class="w-full text-center">
+<div class="w-full flex flex-wrap items-center justify-center">
   <h1
-    class="p-7 text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2"
+    class="p-8 text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2"
   >
     Soutenir le projet
   </h1>
+  <div class="w-full text-center">
+    <label class="p-2" for="affiliationCode">Code d'affiliation :</label>
+    <input
+    class="text-black opacity-30 hover:opacity-100 max-w-[12rem] w-4/5"
+    value={affiliationCode}
+    type="text"
+    name="affiliationCode"
+    on:input={debounce(async (e) => {
+      affiliationCode = e.target.value;
+    }, userDelay * 4)}
+    on:keydown={debounce(async (e) => {
+      if ("Enter" == e.key) {
+        if (document.activeElement instanceof HTMLElement)
+          document.activeElement.blur();
+        e.target.blur();
+        // TODO : auto trigger href link
+      }
+    }, userDelay)}
+    />
+  </div>
+
   <!-- // TODO : affiliation code input auto fill for paypal reference trakings ? -->
   <a
-  class="p-12 text-2xl hover:cursor-pointer hover:no-underline"
-  href="https://www.monwoo.com/don" target="_blank"> www.monwoo.com/don </a>
+    class="p-7 text-2xl hover:cursor-pointer hover:no-underline"
+    href={`https://www.monwoo.com/don${
+      affiliationCode
+      ? `?affiliationCode=${encodeURIComponent(affiliationCode)}`
+      : ``
+    }`}
+    target="_blank"
+  >
+    { affiliationCode ? affiliationCode + ' : ' : ''}
+    www.monwoo.com/don
+  </a>
 </div>
