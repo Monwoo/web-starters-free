fa54c503f761ff5e52e54bd406ce75ab0e08b2da
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2024-04-09 03:14:15 +0200

    [MWS][mws-moon-manager][mws-pdf-billings] in progress, improving backup

--- apps/mws-sf-pdf-billings/backend/src/Controller/FactoryResetController.php
+++ apps/mws-sf-pdf-billings/backend/src/Controller/FactoryResetController.php
@@ -50,23 +50,44 @@ class FactoryResetController extends AbstractController
         } else {
             // TODO : timestamp file is better, any db write will postpone timer with current way :
             // https://stackoverflow.com/questions/13386082/filemtime-warning-stat-failed-for
-            $db_creation_timestamp = filectime(realpath($database));
+            // $db_creation_timestamp = filectime(realpath($database)); TIPS : not accruate, will update on any DB operation...
+            $timestamp = time();
+            $lastTimestampFile = "$projectDir/var/factory-reset-timestamp.txt";
+            $lastTimestamp = null;
+            if (file_exists($lastTimestampFile)) {
+                $lastTimestamp = intval(file_get_contents($lastTimestampFile));
+            }
+            $minDelay = 60; // attente minimum de 1 minute avant reset
+            $next_possible_reset_date->setTimestamp($lastTimestamp ?? $timestamp);
+            $next_possible_reset_date->add(new \DateInterval('PT1M'));
+
+            $lastBckupDelta = $lastTimestamp
+            ? $timestamp - $lastTimestamp
+            : $minDelay;
+            if ($lastBckupDelta < $minDelay) {
+                $didFail = true;
+                // TODO : e2e tests on user messages realistic with timings ? (using time stubs etc...)
+                $msg .= 'ERROR : Please wait at most 1 minutes before next factory reset is available. ';
+            } else {
+                $clean();
+                file_put_contents($lastTimestampFile, $timestamp);
+            }
             // $stat = stat($database);
             // $db_creation_timestamp = $stat['ctime']; // date_create(date("Y-m-d", $stat['ctime']));
 
-            $next_possible_reset_date->setTimestamp($db_creation_timestamp);
-            // TIPS : allow factory reset only each 1 minutes to avoid service cleanups overloads
-            $next_possible_reset_date->add(new \DateInterval('PT1M'));
-            if (
-                ($serverClock)->getTimestamp()
-                > $next_possible_reset_date->getTimestamp()
-            ) {
-                $clean();
-            } else {
-                $didFail = true;
-                // TODO : e2e tests on user messages realistic with timings ? (using time stubs etc...)
-                $msg .= 'ERROR : Please wait at most 5 minutes before next factory reset is available. ';
-            }
+            // $next_possible_reset_date->setTimestamp($db_creation_timestamp);
+            // // TIPS : allow factory reset only each 1 minutes to avoid service cleanups overloads
+            // $next_possible_reset_date->add(new \DateInterval('PT1M'));
+            // if (
+            //     ($serverClock)->getTimestamp()
+            //     > $next_possible_reset_date->getTimestamp()
+            // ) {
+            //     $clean();
+            // } else {
+            //     $didFail = true;
+            //     // TODO : e2e tests on user messages realistic with timings ? (using time stubs etc...)
+            //     $msg .= 'ERROR : Please wait at most 5 minutes before next factory reset is available. ';
+            // }
         }
 
         return $this->json([
