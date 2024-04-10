af0b73217cdde0f8b7e8227e70347c72a42393cd
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2024-04-10 03:45:00 +0200

    [MWS][mws-moon-manager][mws-pdf-billings] in progress, allow private mws-demo bundle to exist of not

--- apps/mws-sf-pdf-billings/backend/assets/svelte/controllers/MwsDemoWidget.svelte
+++ apps/mws-sf-pdf-billings/backend/assets/svelte/controllers/MwsDemoWidget.svelte
@@ -1,7 +1,19 @@
 <script>
-  import MwsDemoWidget from "mws-demo/assets/svelte/controllers/MwsDemoWidget.svelte";
+import { onMount } from 'svelte';
+
+
+  // import MwsDemoWidget from "mws-demo/assets/svelte/controllers/MwsDemoWidget.svelte";
   export let locale;
   export let pageViews = 0;
+
+  let MwsDemoWidget;
+  onMount(async () => {
+    MwsDemoWidget = (await import('mws-demo/assets/svelte/controllers/MwsDemoWidget.svelte')).default ?? null;
+  });
+
 </script>
 
-<MwsDemoWidget {locale} {pageViews} />
+<!-- <MwsDemoWidget {locale} {pageViews} /> -->
+<svelte:component this={MwsDemoWidget} {locale} {pageViews}>
+  <p>some slotted content</p>
+</svelte:component>
