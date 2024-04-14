47738a5d5fd8d70c7c076704bf43c13f15712ca5
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2024-04-14 09:24:02 +0200

    [MWS][mws-moon-manager][mws-pdf-billings] in progress, improving backup UX

--- packages/mws-moon-manager/src/Command/BackupCommand.php
+++ packages/mws-moon-manager/src/Command/BackupCommand.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 #[AsCommand(
     name: 'mws:backup',
@@ -40,31 +41,49 @@ class BackupCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // $this->userLogin = $input->getOption("userLogin");
-        // $minBackupDelay = 24 * 60 * 60; // attente minimum de 24 heures entre chaque backups...
-        // $minBackupDelay = 5; // attente minimum de 5 secondes
-        $minBackupDelay = 60; // attente minimum de 1 minute
-
         $cmdStatus = Command::SUCCESS;
-
+        // $backupStack = $this->params->get('mws.config.bckup.maxStack');
+        $backupStack = json_decode($_SERVER['CONFIG_BCKUP_MAX_STACK'] ?? -1);
+        if ($backupStack < 0) {
+            $backupStack = PHP_INT_MAX;
+        }
+        if ($backupStack === 0) {
+            $output->writeln([
+                "<info>Backup deactivated, check 'CONFIG_BCKUP_MAX_STACK'</info>",
+            ]);
+            return $cmdStatus;
+        }
         $projectDir = $this->params->get('kernel.project_dir');
         $backupsDir = "$projectDir/bckup";
         $timestamp = time();
-
-        // check last backup timestamp (faster to read/write than than sorting possible huge backup dir folder....)
-        $lastBackupTimestamp = 0;
         $lastTimestampFile = "$backupsDir/timestamp.txt";
-        if (file_exists($lastTimestampFile)) {
-            $lastBackupTimestamp = intval(file_get_contents($lastTimestampFile));
-        }
-        $lastBckupDelta = $lastBackupTimestamp
-        ? $timestamp - $lastBackupTimestamp
-        : $minBackupDelay;
-        if ($lastBckupDelta < $minBackupDelay) {
-            // dd($lastBckupDelta);
-            // backup already done, no need to redo.
-            return $cmdStatus;
-        }
+        // if (file_exists($lastTimestampFile)) { // TIPS : use from LISTENER...
+        //     $lastBackupTimestamp = intval(file_get_contents($lastTimestampFile));
+        // }
+
+        // $this->userLogin = $input->getOption("userLogin");
+        // $minBackupDelay = 24 * 60 * 60; // attente minimum de 24 heures entre chaque backups...
+        // $minBackupDelay = 5; // attente minimum de 5 secondes
+        // $minBackupDelay = 60; // attente minimum de 1 minute
+        // TODO : wrong optimisation ? Can MISS backups if delay bigger
+        //        than data updates....
+        // // TODO : refactor : delay usable in LISTENER to trigger cmd instead...
+        // $minBackupDelay = json_decode($_SERVER['CONFIG_BCKUP_AUTOSAVE_MIN_DELAY'] ?? -1);
+        // // check last backup timestamp (faster to read/write than than sorting possible huge backup dir folder....)
+        // $lastBackupTimestamp = 0;
+        // $lastTimestampFile = "$backupsDir/timestamp.txt";
+        // if (file_exists($lastTimestampFile)) {
+        //     $lastBackupTimestamp = intval(file_get_contents($lastTimestampFile));
+        // }
+        // $lastBckupDelta = $lastBackupTimestamp
+        // ? $timestamp - $lastBackupTimestamp
+        // : $minBackupDelay;
+        // if ($lastBckupDelta < $minBackupDelay) {
+        //     // dd($lastBckupDelta);
+        //     // backup already done, no need to redo.
+        //     // TODO : wrong meaning for bckup tool ? will miss data if done before wipe out...
+        //     return $cmdStatus;
+        // }
         $backupFolderName = (new DateTime())
         ->setTimestamp($timestamp)->format('Ymd_His');
         $currentBackupDir = "$backupsDir/$backupFolderName";
@@ -90,10 +109,36 @@ class BackupCommand extends Command
 
         // TODO : remove and clean copy olders than 60 days ? (to keep spaces...)
 
+        file_put_contents($lastTimestampFile, $timestamp);
         $output->writeln([
             "<info>Did backup to '$backupDatabaseFile'</info>",
         ]);
-        file_put_contents($lastTimestampFile, $timestamp);
+
+        // Clean up too olds backups :
+        $backupsDir = "$projectDir/bckup";
+        $finder = [];
+        if (!empty(glob($backupsDir))) {
+            $finder = new Finder();
+            $finder->directories()->in($backupsDir)
+                ->ignoreDotFiles(true)
+                ->ignoreUnreadableDirs()
+                ->depth(0);
+
+            $finder->sort(function (SplFileInfo $a, SplFileInfo $b): int {
+                // return strcmp($a->getRealPath(), $b->getRealPath());
+                return strcmp($b->getRealPath(), $a->getRealPath());
+            });
+            $finder = iterator_to_array($finder, false);
+        }
+
+        $toClean = array_slice($finder, $backupStack);
+        $output->writeln([
+            "<info>Backup to clean : </info>" . count($toClean),
+        ]);
+        /** @var SplFileInfo $f */
+        foreach ($toClean as $f) {
+            $this->filesystem->remove($f->getPathname());
+        }
 
         return $cmdStatus;
     }
