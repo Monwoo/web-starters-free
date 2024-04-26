4c9bdd1e21e854c690ee7a1b55c0d10fd4ef1945
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2024-04-26 02:33:53 +0200

    [MWS][mws-moon-manager][mws-pdf-billings] in progress, improving tests

---
+++ apps/mws-sf-pdf-billings/backend/tests/e2e/01-failures/todo.cest.php
@@ -0,0 +1,6 @@
+// TODO : secu tests : no access to roles based routes if not allowed
+
+// upload injection tests of harmfull php in uploads
+// or harmfull js in html sanitized field (+ test all coverage
+// with html 'input'/'textarea'/'APIs' tags extractors
+// on site maps per user roles (automaic routes listings per role ?))
