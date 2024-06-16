b1abce1c3d454837557a1b1bd8313d4c9756abde
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2024-06-16 10:13:03 +0200

    [MWS][mws-moon-manager][mws-pdf-billings] in progress, improving migration system

--- packages/mws-moon-manager/src/Command/MigrateDatabaseCommand.php
+++ packages/mws-moon-manager/src/Command/MigrateDatabaseCommand.php
@@ -6,7 +6,6 @@
 
 namespace MWS\MoonManagerBundle\Command;
 
-use Doctrine\Bundle\MigrationsBundle\Command\DoctrineCommand;
 use Doctrine\Bundle\MigrationsBundle\Command\Helper\DoctrineCommandHelper;
 // use Psr\SimpleCache\CacheInterface;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -14,9 +13,16 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand;
-use Doctrine\Migrations\Tools\Console\Command\DoctrineCommand as CommandDoctrineCommand;
+use Doctrine\Migrations\Exception\NoMigrationsFoundWithCriteria;
+use Doctrine\Migrations\Exception\NoMigrationsToExecute;
+use Doctrine\Migrations\Exception\UnknownMigrationVersion;
+use Doctrine\Migrations\Tools\Console\Command\DoctrineCommand;
+use Doctrine\Migrations\Tools\Console\ConsoleRunner;
+use Doctrine\Migrations\Tools\Console\MigratorConfigurationFactory;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 // class MigrateDatabaseCommand extends MigrateCommand
@@ -26,7 +32,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
   hidden: false,
   // aliases: ['backup:sync']
 )]
-class MigrateDatabaseCommand extends CommandDoctrineCommand
+class MigrateDatabaseCommand extends DoctrineCommand // MigrateCommand is final, use as doc...
 {
   // Depreciated : (use annotations ok)
   // protected static $defaultName = 'mws:db:migrate';
@@ -42,24 +48,29 @@ class MigrateDatabaseCommand extends CommandDoctrineCommand
 
   protected function configure(): void
   {
-    parent::configure();
-
     $this
       ->setDescription('Check if a database migration is needed and enable maintenance mode to execute it if there is.')
+      ->addArgument(
+        'version',
+        InputArgument::OPTIONAL,
+        'The version FQCN or alias (first, prev, next, latest) to migrate to.',
+        'latest'
+      )
       ->addOption('db', null, InputOption::VALUE_REQUIRED, 'The database connection to use for this command.')
       ->addOption('em', null, InputOption::VALUE_REQUIRED, 'The entity manager to use for this command.')
       ->addOption('shard', null, InputOption::VALUE_REQUIRED, 'The shard connection to use for this command.');
+    parent::configure();
   }
 
   const MIGRATE_KEY = 'mws.migrationInProgress';
   // const MIGRATE_KEY = 'mws.maintenanceInProgress';
   const MAINTENANCE_KEY = 'mws.maintenanceInProgress'; // mws. prefix ?
-  public function execute(InputInterface $input, OutputInterface $output)
+  public function execute(InputInterface $input, OutputInterface $output): int
   {
     $io = new SymfonyStyle($input, $output);
 
     // $productsCount = $this->cache->getItem(self::MIGRATE_KEY);
-    // $this->cache->delete(self::MIGRATE_KEY); // TODO : option to force lock release in case of buggy command missing release...
+    $this->cache->delete(self::MIGRATE_KEY); // TODO : option to force lock release in case of buggy command missing release...
 
     if ($this->cache->get(self::MIGRATE_KEY, function ($item) {
       return false;
@@ -82,21 +93,58 @@ class MigrateDatabaseCommand extends CommandDoctrineCommand
     // Check whether there are migrations to execute or not
     $io->text('Loading migrations');
 
-    $app = $this->getApplication();
-    // TODO : replacement for legacy ? or missing package ?
-    // "Doctrine\Bundle\DoctrineBundle\Command\DoctrineCommand not found...
-    DoctrineCommand::setApplicationHelper($app, $input);
+    // $app = $this->getApplication();
+    // // TODO : replacement for legacy ? or missing package ?
+    // // "Doctrine\Bundle\DoctrineBundle\Command\DoctrineCommand not found...
+    // CommandDoctrineCommand::setApplicationHelper($app, $input);
+    // DoctrineCommand::configureMigrations(
+    //   $app->getKernel()->getContainer(),
+    //   $this->getMigrationConfiguration($input, $output)
+    // );
+    // $configuration = $this->getMigrationConfiguration($input, $output);
+    // $toExecute = array_diff(
+    //   $configuration->getAvailableVersions(),
+    //   $configuration->getMigratedVersions()
+    // );
+    $versionAlias     = $input->getArgument('version');
+    // TODO : not working here but ok in doctrine final class command ?
+    // $df = ConsoleRunner::findDependencyFactory();
+    $df = $this->getDependencyFactory();
 
-    DoctrineCommand::configureMigrations(
-      $app->getKernel()->getContainer(),
-      $this->getMigrationConfiguration($input, $output)
-    );
+    $migratorConfigurationFactory = $df->getConsoleInputMigratorConfigurationFactory();
+    $migratorConfiguration        = $migratorConfigurationFactory->getMigratorConfiguration($input);
+    $migratorConfiguration->isDryRun();
+    $databaseName = (string) $df->getConnection()->getDatabase();
+    $df->getMetadataStorage()->ensureInitialized();
+    $migrationRepository = $df->getMigrationRepository();
+    if (count($migrationRepository->getMigrations()) === 0) {
+      $message = sprintf(
+        'The version "%s" couldn\'t be reached, there are no registered migrations.',
+        $versionAlias
+      );
+      $this->io->warning($message);
+      return Command::FAILURE;
+    }
 
-    $configuration = $this->getMigrationConfiguration($input, $output);
-    $toExecute = array_diff(
-      $configuration->getAvailableVersions(),
-      $configuration->getMigratedVersions()
-    );
+    // https://vscode.dev/github/Monwoo/web-starters-free/blob/main/packages/mws-moon-manager/vendor/doctrine/migrations/lib/Doctrine/Migrations/Tools/Console/Command/MigrateCommand.php#L261
+    try {
+      $version = $df->getVersionAliasResolver()->resolveVersionAlias($versionAlias);
+    } catch (UnknownMigrationVersion $e) {
+      $this->io->error(sprintf(
+        'Unknown version: %s',
+        OutputFormatter::escape($versionAlias)
+      ));
+      // return 1;
+    } catch (NoMigrationsToExecute | NoMigrationsFoundWithCriteria $e) {
+      // return $this->exitForAlias($versionAlias);
+    }
+
+    $toExecute = false;
+    if ($version) {
+      $planCalculator = $df->getMigrationPlanCalculator();
+      $plan = $planCalculator->getPlanUntilVersion($version);
+      $toExecute = count($plan) === 0;
+    }
 
     if (!$toExecute) {
       // No migration to execute: do not enable maintenance
